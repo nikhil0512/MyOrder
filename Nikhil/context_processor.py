ac9be2ef4db8030d919a1f05672ae588740221d1
Nikhil <gupta.nikhil.0512@gmail.com>
2020-05-26 00:59:39 +0530

    set SITE_URL in context_processor.

--- Nikhil/context_processor.py
+++ Nikhil/context_processor.py
@@ -1,7 +1,7 @@
-from django.conf import settings
+import os
 
 def root_url(request):
     """
     Pass your root_url from the settings.py
     """
-    return {'SITE_URL': settings.BASE_DIR}
+    return {'SITE_URL': os.path.dirname(os.path.abspath(__file__))}
