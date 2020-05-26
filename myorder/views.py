eb147d3e33f46dc434405cf8330fa52dc7438714
Nikhil <gupta.nikhil.0512@gmail.com>
2020-05-26 19:21:55 +0530

    Add Pari email id and remove base url string.

--- myorder/views.py
+++ myorder/views.py
@@ -79,6 +79,6 @@ def send_order(username, phone, address, file_path):
 mobile no - {}
 Address - {}'''.format(username, phone, address)
     recepient = 'nikhilrajeevgupta@gmail.com'
-    email = EmailMessage(subject, message, EMAIL_HOST_USER, [recepient])
+    email = EmailMessage(subject, message, EMAIL_HOST_USER, [recepient, 'sketchcraftstudio@gmail.com'])
     email.attach_file(file_path)
     email.send()
