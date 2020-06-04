0ca4f11ef560589493200dd6e9fc20486cd64a44
Nikhil <gupta.nikhil.0512@gmail.com>
2020-06-04 23:20:47 +0530

    Password add.

--- Nikhil/settings.py
+++ Nikhil/settings.py
@@ -132,7 +132,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'aktraders0731@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'Aktraders@2020'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
