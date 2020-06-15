ffe76c125bc43ee7513b73878b8ced681990e503
Nikhil <gupta.nikhil.0512@gmail.com>
2020-06-15 15:36:40 +0530

    commit settings file.

--- Nikhil/settings.py
+++ Nikhil/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5a*d@%x2-%#06g7(vf7z^yt0lkxny2rf0)&%bow#n1@l6wxujz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://placeyourorders.herokuapp.com/']
 
